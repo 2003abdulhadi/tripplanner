2189bac8d2e3261af29c36de9456946d30304899
Abdul Hadi <2003abdulhadi@gmail.com>
2025-06-27 01:31:22 -0400

    Stick add item button to bottom right

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,8 @@
 import prisma from "@/utils/prisma/prisma";
 import { createClient } from "@/utils/supabase/server";
 import { ItemTable } from "./components/item-table";
-import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
 import { ItemRow, ProviderRow } from "@/lib/types";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import { createItem } from "./components/actions";
+import { AddItemButton } from "./components/add-item";
 
 export default async function HomePage() {
   const supabase = await createClient();
@@ -55,63 +37,11 @@ export default async function HomePage() {
       <div className="relative flex-auto flex">
         <ItemTable
           itemCategories={itemCategories}
-          items={items}
+          items={[...items, ...items, ...items]}
           currentUser={user?.id}
         />
 
-        {user ? (
-          <Dialog>
-            <DialogTrigger asChild>
-              <Button
-                className="absolute bottom-4 right-4 z-10 rounded-full p-4 shadow-lg w-0 h-0 font-black"
-                aria-label="Add item"
-              >
-                +
-              </Button>
-            </DialogTrigger>
-            <DialogContent>
-              <DialogHeader>
-                <DialogTitle>Add New Item</DialogTitle>
-                <DialogDescription>
-                  Create a new item to add to the list
-                </DialogDescription>
-                <form action={createItem} className="flex flex-col gap-2">
-                  <Label htmlFor="name">Item Name: </Label>
-                  <Input id="name" type="text" name="name" required />
-                  <Label htmlFor="needed">Quantity Needed: </Label>
-                  <Input
-                    id="needed"
-                    type="number"
-                    name="needed"
-                    min={0}
-                    defaultValue={0}
-                    required
-                  />
-                  <Label htmlFor="itemCategorySlug">Category: </Label>
-                  <Select name="itemCategorySlug" required>
-                    <SelectTrigger className="w-full">
-                      <SelectValue placeholder="Select a category" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      {itemCategories.map(({ slug, label }) => (
-                        <SelectItem key={slug} value={slug}>
-                          {label}
-                        </SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
-                  <Label htmlFor="note">
-                    Note <em>(optional):</em>
-                  </Label>
-                  <Input id="note" type="text" name="note" />
-                  <Button>Add Item</Button>
-                </form>
-              </DialogHeader>
-            </DialogContent>
-          </Dialog>
-        ) : (
-          <></>
-        )}
+        {user ? <AddItemButton categories={itemCategories} /> : <></>}
       </div>
     </div>
   );
