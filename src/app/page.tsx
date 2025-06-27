952769f2dc67f50541cc32b7f2d9acc2cff0aa41
Abdul Hadi <2003abdulhadi@gmail.com>
2025-06-27 01:35:35 -0400

    remove debug duplication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default async function HomePage() {
       <div className="relative flex-auto flex">
         <ItemTable
           itemCategories={itemCategories}
-          items={[...items, ...items, ...items]}
+          items={[...items]}
           currentUser={user?.id}
         />
 
