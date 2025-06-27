8114b004cdaaf60d46528cfe13925329a80807fe
Abdul Hadi <2003abdulhadi@gmail.com>
2025-06-26 21:09:21 -0400

    prisma logging

--- src/utils/prisma/prisma.ts
+++ src/utils/prisma/prisma.ts
@@ -1,10 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
+console.log(PrismaClient);
+
 declare global {
-	var prisma: PrismaClient | undefined;
+  var prisma: PrismaClient | undefined;
 }
 
 const prisma = global.prisma || new PrismaClient({ log: ["info"] });
+console.log(prisma);
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+console.log(prisma);
 
 export default prisma;
